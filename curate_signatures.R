31c7689ad7d2cf17a391409ea29703305b0a0282
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-04-13 23:36:04 -0400

    Made the cell_line calculation repeatable. Now it automaticall finds the common cell lines between given drugs. The subsequent index file for signatures is also regenerated

--- curate_signatures.R
+++ curate_signatures.R
@@ -1,34 +1,43 @@
 library(tidyverse)
 
-cell_lines <- c("A375", "HA1E", "MCF7", "PC3")
+carbetocin <- read_tsv("signature_data/Carbetocin-Signatures.tsv")
 
-carbetocin <- read_tsv("signature_data/Carbetocin-Signature.tsv") %>% 
+desmopressin <- read_tsv("signature_data/Desmopressin-Signatures.tsv")
+
+hydroxychloroquine <- read_tsv("signature_data/Hydroxychloroquine-Signatures.tsv")
+
+chloroquine <- read_tsv("signature_data/Chloroquine-Signatures.tsv")
+
+bupropion <- read_tsv("signature_data/Bupropion-Signatures.tsv")
+
+lopinavir <- read_tsv("signature_data/Lopinavir-Signatures.tsv")
+
+ritonavir <- read_tsv("signature_data/Ritonavir-Signatures.tsv")
+
+drugs_list <- list(bupropion, carbetocin, chloroquine, desmopressin, lopinavir, ritonavir)
+selected_lines <- list()
+
+for (i in 1:length(drugs_list)) {
+  selected_lines[[i]] <- drugs_list[[i]]$CellLine
+}
+
+cell_lines <- reduce(selected_lines, intersect)
+
+IL6 <- read_tsv("signature_data/IL6-Signatures.tsv")
+
+IL6R <- read_tsv("signature_data/IL6-Signatures.tsv")
+
+IL6ST <- read_tsv("signature_data/IL6-Signatures.tsv")
+
+drugs <- bind_rows(carbetocin, desmopressin, hydroxychloroquine, chloroquine, bupropion) %>% 
   filter(CellLine %in% cell_lines)
 
-desmopressin <- read_tsv("signature_data/Desmopressin-Signature.tsv") %>% 
-  filter(CellLine %in% cell_lines)
-
-hydroxychloroquine <- read_tsv("signature_data/Hydroxychloroquine-Signatures.tsv") %>% 
-  filter(CellLine %in% cell_lines)
-
-chloroquine <- read_tsv("signature_data/Chloroquine-Signatures.tsv") %>% 
-  filter(CellLine %in% cell_lines)
-
-bupropion <- read_tsv("signature_data/Bupropion-Signatures.tsv") %>% 
-  filter(CellLine %in% cell_lines)
-
-IL6 <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
-  filter(TargetGene == "IL6", CellLine %in% cell_lines)
-
-IL6R <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
-  filter(TargetGene == "IL6R", CellLine %in% cell_lines)
-
-IL6ST <- read_tsv("signature_data/IL6-Signatures.tsv") %>% 
-  filter(TargetGene == "IL6ST", CellLine %in% cell_lines)
-
-drugs <- bind_rows(carbetocin, desmopressin, hydroxychloroquine, chloroquine, bupropion)
 write_file(paste(drugs$SignatureId, collapse = "\n"), "drugs_signature_ids")
-targets <- bind_rows(IL6, IL6R, IL6ST)
+
+targets <- bind_rows(IL6, IL6R, IL6ST)  %>% 
+  filter(CellLine %in% cell_lines)
+
 write_file(paste(targets$SignatureId, collapse = "\n"), "targets_signature_ids")
+
 drugs %>% select(SignatureId, Perturbagen, CellLine) %>% 
   write_csv("signature_data/id-name-cellline_mapping.csv")
