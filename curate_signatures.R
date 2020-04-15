f4354b69bc8df2dfb735ba403d9b2bf77b174918
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-04-14 21:57:54 -0400

    New drugs added to the mix

--- curate_signatures.R
+++ curate_signatures.R
@@ -23,21 +23,10 @@ for (i in 1:length(drugs_list)) {
 
 cell_lines <- reduce(selected_lines, intersect)
 
-IL6 <- read_tsv("signature_data/IL6-Signatures.tsv")
-
-IL6R <- read_tsv("signature_data/IL6-Signatures.tsv")
-
-IL6ST <- read_tsv("signature_data/IL6-Signatures.tsv")
-
-drugs <- bind_rows(carbetocin, desmopressin, hydroxychloroquine, chloroquine, bupropion) %>% 
+drugs <- bind_rows(carbetocin, desmopressin, hydroxychloroquine, chloroquine, bupropion, lopinavir, ritonavir) %>% 
   filter(CellLine %in% cell_lines)
 
 write_file(paste(drugs$SignatureId, collapse = "\n"), "drugs_signature_ids")
-
-targets <- bind_rows(IL6, IL6R, IL6ST)  %>% 
-  filter(CellLine %in% cell_lines)
-
-write_file(paste(targets$SignatureId, collapse = "\n"), "targets_signature_ids")
 
 drugs %>% select(SignatureId, Perturbagen, CellLine) %>% 
   write_csv("signature_data/id-name-cellline_mapping.csv")
