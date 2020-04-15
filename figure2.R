4c7dc5d3cee3fb6391dc776b1087b53bf222f4e1
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2020-04-14 21:59:27 -0400

    Added the global heatmaps.

--- figure2.R
+++ figure2.R
@@ -1,24 +1,28 @@
 library(tidyverse)
 library(RColorBrewer)
-library(gplots)
 library(reshape2)
 
 all_averaged <- read_csv("results/all_averaged.csv")
 
-all_averaged_cross_inf <- all_averaged %>% 
-  select(IL1A, IL1B, IL6, IL6R, IL6ST, TNF) %>% 
+all_averaged_cross <- all_averaged %>% 
   dcast(perturbagen ~ treatment, value.var = "mean_similarity") %>% 
+  column_to_rownames("perturbagen") %>% 
+  as.matrix()
+
+all_averaged_cross_inf <- all_averaged %>% 
+  dcast(perturbagen ~ treatment, value.var = "mean_similarity") %>% 
+  select_if(names(.) %in% c("IL1A", "IL1B", "IL1R1", "IL1R2", "IL1RN",
+                            "IL1RAP", "IL1RL1", "IL1RL2", "IL6", "IL6R", "IL6ST", "TNF", "perturbagen")) %>% 
   column_to_rownames("perturbagen") %>% 
   as.matrix()
 
 all_averaged_cross_imm <- all_averaged %>% 
-  select(ARG1, CD19, TLR7, TLR9, CD40, CD44, CD46, CTLA4) %>% 
   dcast(perturbagen ~ treatment, value.var = "mean_similarity") %>% 
+  select_if(names(.) %in% c("ARG1", "CD19", "TLR7", "TLR9", "CD40", "CD44", "CD46", "CTLA4", "perturbagen")) %>% 
   column_to_rownames("perturbagen") %>% 
   as.matrix()
 
-colors <- rev(brewer.pal(8, "RdBu"))
-
+colors <- colorRampPalette(c("red", "black", "green"))(n=11)
 
 png(filename = "figures/average-concordance-heatmap-inflammation.png", width = 1920, height = 1384)
 heatmap(all_averaged_cross_inf, Colv = NA, Rowv = NA, scale = "column", col = colors)
@@ -26,4 +30,8 @@ dev.off()
 
 png(filename = "figures/average-concordance-heatmap-immune.png", width = 1920, height = 1384)
 heatmap(all_averaged_cross_imm, Colv = NA, Rowv = NA, scale = "column", col = colors)
+dev.off()
+
+png(filename = "figures/average-concordance-heatmap-all.png", width = 1920, height = 1384)
+heatmap(all_averaged_cross, Colv = NA, Rowv = NA, scale = "column", col = colors)
 dev.off()
--- figure3.R
+++ figure3.R
@@ -1,24 +1,31 @@
 library(tidyverse)
 library(RColorBrewer)
-library(gplots)
 library(reshape2)
 
 all_results <- read_csv("results/all_results.csv")
 
-all_results_cross_inf <- all_results %>% 
-  select(IL1A, IL1B, IL6, IL6R, IL6ST, TNF, -cellline) %>% 
+all_results_cross <- all_results %>% 
+  select(-cellline) %>% 
   dcast(perturbagen ~ treatment, value.var = "similarity") %>% 
+  column_to_rownames("perturbagen") %>% 
+  as.matrix()
+
+all_results_cross_inf <- all_results %>% 
+  select(-cellline) %>% 
+  dcast(perturbagen ~ treatment, value.var = "similarity") %>% 
+  select_if(names(.) %in% c("IL1A", "IL1B", "IL1R1", "IL1R2", "IL1RN",
+                            "IL1RAP", "IL1RL1", "IL1RL2", "IL6", "IL6R", "IL6ST", "TNF", "perturbagen")) %>% 
   column_to_rownames("perturbagen") %>% 
   as.matrix()
 
 all_results_cross_imm <- all_results %>% 
-  select(ARG1, CD19, TLR7, TLR9, CD40, CD44, CD46, CTLA4, -cellline) %>% 
+  select(-cellline) %>% 
   dcast(perturbagen ~ treatment, value.var = "similarity") %>% 
+  select_if(names(.) %in% c("ARG1", "CD19", "TLR7", "TLR9", "CD40", "CD44", "CD46", "CTLA4", "perturbagen")) %>% 
   column_to_rownames("perturbagen") %>% 
   as.matrix()
 
-colors <- rev(brewer.pal(8, "RdBu"))
-
+colors <- colorRampPalette(c("red", "black", "green"))(n=11)
 
 png(filename = "figures/max-concordance-heatmap-inflammation.png", width = 1920, height = 1384)
 heatmap(all_results_cross_inf, Colv = NA, Rowv = NA, scale = "column", col = colors)
@@ -28,3 +35,6 @@ png(filename = "figures/max-concordance-heatmap-immune.png", width = 1920, heigh
 heatmap(all_results_cross_imm, Colv = NA, Rowv = NA, scale = "column", col = colors)
 dev.off()
 
+png(filename = "figures/max-concordance-heatmap-all.png", width = 1920, height = 1384)
+heatmap(all_results_cross, Colv = NA, Rowv = NA, scale = "column", col = colors)
+dev.off()
